e07f4360ea3dff01a7840c12337c1b8ec9b61879
Julian Parsert <julian.parsert@student.uibk.ac.at>
2017-03-18 22:05:09 +0100

    added file and others tuff

--- FaceBlur.py
+++ FaceBlur.py
@@ -6,12 +6,14 @@ from multiprocessing import Pool
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
 
+from optparse import OptionParser
+
 import cv2
 import face_recognition
 
 class FaceBlur(object):
 
-    def __init__(self, input_dir='images' , max_blurring_iterations=5, result_dir=None):
+    def __init__(self, input_dir, result_dir, file=None, max_blurring_iterations=5,):
         """
         Constructor of FaceBlur Object
         :param input_dir: directory containing images to process
@@ -20,8 +22,11 @@ class FaceBlur(object):
         """
 
         self._max_blurring_iterations = max_blurring_iterations
+        if input_dir is None:
+            self.file = file
         self.input_dir = input_dir
-        if result_dir is None:
+
+        if result_dir is None and (input_dir is not None):
             self.result_dir=input_dir+"_blurred"
         else:
             self.result_dir = result_dir
@@ -49,6 +54,12 @@ class FaceBlur(object):
         """
         return self.blur_face(*args)
 
+    def write_image(self, image_name , image):
+        if self.input_dir is None :
+            cv2.imwrite("blur_{}".format(image_name), image)
+        else:
+            cv2.imwrite(join(self.result_dir, image_name), image)
+
     def detect_and_blur_faces(self, image_name):
         """
         Detect faces and blur them in image_name
@@ -56,7 +67,11 @@ class FaceBlur(object):
         :return: processed image
         """
 
-        image = cv2.imread(os.path.join(self.input_dir,image_name))
+        if self.input_dir is None :
+            image = cv2.imread(image_name)
+        else:
+            image = cv2.imread(os.path.join(self.input_dir,image_name))
+
         print(image_name)
         face_locs = face_recognition.face_locations(image)
 
@@ -86,8 +101,7 @@ class FaceBlur(object):
                 left = max(left, 0)
                 image[top:bottom, left:right]=i
 
-        cv2.imwrite(join(self.result_dir, image_name), image)
-
+        self.write_image(image_name,image)
 
     def detect_and_blur_helper(self, args):
         """
@@ -102,10 +116,14 @@ class FaceBlur(object):
         Starts processing
         """
 
-        all_imgs = [f for f in os.listdir(self.input_dir) if isfile(join(self.input_dir, f))]
+        if self.input_dir is None:
+            all_imgs = [self.file]
+        else:
+            all_imgs = [f for f in os.listdir(self.input_dir) if isfile(join(self.input_dir, f))]
 
-        if not os.path.exists(self.result_dir):
-            os.makedirs(self.result_dir)
+        if self.input_dir is not None:
+            if not os.path.exists(self.result_dir):
+                os.makedirs(self.result_dir)
 
         with Pool() as p:
             list = [(img) for img in all_imgs]
@@ -113,10 +131,17 @@ class FaceBlur(object):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        arg = "images/"
-    else:
-        arg = sys.argv[1]
+    parser = OptionParser()
+    parser.add_option("-i","--input" , dest="input_dir", help="Directory containing images to be processed")
+    parser.add_option("-f", "--file" , dest="file", help="File to process")
+    parser.add_option("-o","--output" , dest="output_dir", help="Directory to save results" , default=None)
+    parser.add_option("-n", "--maxblur", dest="maxblur", help="Maximal iterations of blur atempt", default=5)
+    options,args = parser.parse_args()
+    if (options.input_dir is None) and (options.file is None):
+        print("Missing argument input directory")
+        parser.print_help()
+        exit()
 
-    fb = FaceBlur(arg)
+
+    fb = FaceBlur(options.input_dir, options.output_dir, file=options.file, max_blurring_iterations=options.maxblur)
     fb.start_processing()
